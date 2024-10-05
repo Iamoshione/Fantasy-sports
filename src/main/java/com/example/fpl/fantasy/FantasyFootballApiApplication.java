c4bf159a9b09ef0960a547e1d73eae06bb37e1ee
iamoshione <oshioneesiemokhai@gmail.com>
2024-10-04 20:28:11 -0400

    ignore files

--- src/main/java/com/example/fpl/fantasy/FantasyFootballApiApplication.java
+++ src/main/java/com/example/fpl/fantasy/FantasyFootballApiApplication.java
@@ -1,16 +1,14 @@
 package com.example.fpl.fantasy;
 
-import com.example.fpl.fantasy.controller.http.HttpRequestManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class FantasyFootballApiApplication {
 
 	public static void main(String[] args)  {
 		SpringApplication.run(FantasyFootballApiApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/example/fpl/fantasy/controller/FantasyController.java
+++ src/main/java/com/example/fpl/fantasy/controller/FantasyController.java
@@ -1,7 +1,6 @@
 package com.example.fpl.fantasy.controller;
 
 
-import com.example.fpl.fantasy.controller.http.HttpRequestManager;
 import com.example.fpl.fantasy.element_stats.service.IElementStatsService;
 import com.example.fpl.fantasy.element_types.service.IElementTypesService;
 import com.example.fpl.fantasy.elements.service.IElementsService;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+
 
 @RestController
 @RequestMapping(value = "/fantasy/api")
@@ -30,7 +29,8 @@ public class FantasyController {
     private final ITotalPlayerService iTotalPlayerService;
     private final IEventsService iEventsService;
 
-    HttpRequestManager httpRequestManager = new HttpRequestManager();
+
+
 
 
     public FantasyController(IGameSettingsService iGameSettingsService, IPhasesService iPhasesService, ITeamsService iTeamsService, IElementsService iElementsService, IElementStatsService iElementStatsService, IElementTypesService iElementTypesService, ITotalPlayerService iTotalPlayerService, IEventsService iEventsService) {
@@ -44,58 +44,46 @@ public class FantasyController {
         this.iEventsService = iEventsService;
     }
 
+
     @GetMapping(value = "/get/settings")
-    public ResponseEntity<?> getGameSettingsById() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getGameSettingsById()  {
         return ResponseEntity.ok(iGameSettingsService.readGameSettings(0));
     }
+
     @GetMapping(value = "/get/phases")
-    public ResponseEntity<?> getPhases() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getPhases()  {
         return ResponseEntity.ok(iPhasesService.readPhases());
     }
 
     @GetMapping(value = "/get/teams")
-    public ResponseEntity<?> getTeams() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getTeams()  {
         return ResponseEntity.ok(iTeamsService.readTeams());
     }
 
     @GetMapping(value = "/get/elements")
-    public ResponseEntity<?> getElements() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getElements()  {
         return ResponseEntity.ok(iElementsService.readElements());
     }
+
     @GetMapping(value = "/get/elementstats")
-    public ResponseEntity<?> getElementStats() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getElementStats()  {
         return ResponseEntity.ok(iElementStatsService.readElementStats());
     }
 
     @GetMapping(value = "/get/elementypes")
-    public ResponseEntity<?> getElementTypes() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getElementTypes()  {
         return ResponseEntity.ok(iElementTypesService.readElementTypes());
     }
 
     @GetMapping(value = "/get/totalplayers")
-    public ResponseEntity<?> getTotalPlayers() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getTotalPlayers()  {
         return ResponseEntity.ok(iTotalPlayerService.readTotalPlayersById(1));
     }
 
-
     @GetMapping(value = "/get/events")
-    public ResponseEntity<?> getEvents() throws IOException, InterruptedException {
-        httpRequestManager.httpRequest();
+    public ResponseEntity<?> getEvents() {
         return ResponseEntity.ok(iEventsService.readEvents());
     }
-
-
-
-
-
-
 
 
 }
